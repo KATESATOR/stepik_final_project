c5adeba58f1d20423e0950b571e413906c12b021
KATESATOR <sigmarus47@mail.ru>
2020-02-19 15:52:32 +0300

    Fixed locators for login page

--- pages/locators.py
+++ pages/locators.py
@@ -6,9 +6,9 @@ class MainPageLocators():
 
 
 class LoginPageLocators():
-    LOGIN_USERNAME = (By.CSS_SELECTOR, "//input[@id='id_login-username']")
-    LOGIN_PASSWORD = (By.CSS_SELECTOR, "//input[@id='id_login-password']")
-    REGISTER_USERNAME = (By.CSS_SELECTOR, "//input[@id='id_registration-email']")
-    REGISTER_PASSWORD = (By.CSS_SELECTOR, "//input[@id='id_registration-password1']")
-    REGISTER_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "//input[@id='id_registration-password2']")
+    LOGIN_USERNAME = (By.ID, "id_login-username")
+    LOGIN_PASSWORD = (By.ID, "id_login-password")
+    REGISTER_USERNAME = (By.ID, "id_registration-email")
+    REGISTER_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTER_PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,17 +9,17 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверка на корректный url адрес
         assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
         assert "login" in self.browser.current_url, "word 'login' not in current url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        # проверка, что есть форма логина на странице
         assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "No username field on login page"
         assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "No password field on login page"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        # проверка, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_USERNAME), "No username field on register page"
         assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "No password field on register page"
         assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD_CONFIRM), "No confirm password field on " \
